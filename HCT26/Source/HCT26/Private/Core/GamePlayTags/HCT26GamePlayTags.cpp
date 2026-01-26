423e0839b36f087f28ccddc4c403c619f75ca3e6
Khoi Van <vanthekhoi@gmail.com>
2026-01-26 23:13:03 +0700

    Declare & Define GamePlay Tag

---
+++ HCT26/Source/HCT26/Private/Core/GamePlayTags/HCT26GamePlayTags.cpp
@@ -0,0 +1,6 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/GamePlayTags/HCT26GamePlayTags.h"
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_Character_CanPossess, "Character.CanPossess");
---
+++ HCT26/Source/HCT26/Public/Core/GamePlayTags/HCT26GamePlayTags.h
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Character_CanPossess);
