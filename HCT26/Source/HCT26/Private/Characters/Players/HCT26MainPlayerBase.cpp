716301df38d8d5cbdd95ef95cd49a8692a77e5b6
Khoi Van <vanthekhoi@gmail.com>
2026-01-31 12:12:30 +0700

    Add MainPlayer

---
+++ HCT26/Source/HCT26/Private/Characters/Players/HCT26MainPlayerBase.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/Players/HCT26MainPlayerBase.h"
+
+
+// Sets default values
+AHCT26MainPlayerBase::AHCT26MainPlayerBase()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AHCT26MainPlayerBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHCT26MainPlayerBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AHCT26MainPlayerBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
