4014c3459020bd6adb8a5ef5f24cb004ce59a66c
Khoi Van <vanthekhoi@gmail.com>
2026-02-06 23:32:02 +0700

    Spawn FX (AddDynamic)

---
+++ HCT26/Source/HCT26/Private/Interaction/HCT26Button2.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interaction/HCT26Button2.h"
+
+
+// Sets default values
+AHCT26Button2::AHCT26Button2()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AHCT26Button2::BeginPlay()
+{
+	Super::BeginPlay();
+	StartOverlap.AddDynamic(this, &AHCT26Button2::ExecuteFXOverlap);
+}
+
+// Called every frame
+void AHCT26Button2::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHCT26Button2::ExecuteFXOverlap(AActor* OverlappingActor)
+{
+	if (OverlapEffect != nullptr)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, OverlapEffect, GetActorLocation());
+	}
+}
+
---
+++ HCT26/Source/HCT26/Public/Interaction/HCT26Button2.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HCT26ButtonBase.h"
+#include "NiagaraFunctionLibrary.h"
+#include "HCT26Button2.generated.h"
+
+UCLASS()
+class HCT26_API AHCT26Button2 : public AHCT26ButtonBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AHCT26Button2();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="HoiChoTet|FX_Overlap")
+	TObjectPtr<UNiagaraSystem> OverlapEffect;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	void ExecuteFXOverlap(AActor* OverlappingActor);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
