7b23d071ae7f955e4f78e10c7685185cedb37c1f
Khoi Van <vanthekhoi@gmail.com>
2026-02-06 23:28:28 +0700

    Add ButtonBase class and Declare Signature

---
+++ HCT26/Source/HCT26/Private/Interaction/HCT26ButtonBase.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interaction/HCT26ButtonBase.h"
+
+
+// Sets default values
+AHCT26ButtonBase::AHCT26ButtonBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	// Create root scene component
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+	
+	// Create, parent, and configure box collision component
+	CollisionComponent = CreateDefaultSubobject<UBoxComponent>("CollisionBox");
+	CollisionComponent->SetupAttachment(RootComponent);
+	CollisionComponent->SetBoxExtent(FVector(50.0f, 50.0f, 50.0f));
+	CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CollisionComponent->SetCollisionObjectType(ECC_WorldDynamic);
+	CollisionComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	CollisionComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	CollisionComponent->SetGenerateOverlapEvents(true);
+}
+
+// Called when the game starts or when spawned
+void AHCT26ButtonBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Bind overlap events
+	CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &AHCT26ButtonBase::OnOverlapBegin);
+	CollisionComponent->OnComponentEndOverlap.AddDynamic(this, &AHCT26ButtonBase::OnOverlapEnd);
+}
+
+// Called every frame
+void AHCT26ButtonBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHCT26ButtonBase::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	StartOverlap.Broadcast(OtherActor);
+}
+
+void AHCT26ButtonBase::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	EndOverlap.Broadcast(OtherActor);
+}
+
---
+++ HCT26/Source/HCT26/Public/Interaction/HCT26ButtonBase.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "HCT26ButtonBase.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSignatureStartOverlap, AActor*, OverlappingActor);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSignatureEndOverlap, AActor*, OverlappingActor);
+
+UCLASS()
+class HCT26_API AHCT26ButtonBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AHCT26ButtonBase();
+	
+	UPROPERTY(BlueprintAssignable, Category=HoiChoTet)
+	FSignatureStartOverlap StartOverlap;
+	
+	UPROPERTY(BlueprintAssignable, Category=HoiChoTet)
+	FSignatureEndOverlap EndOverlap;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USceneComponent> SceneRoot;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UBoxComponent> CollisionComponent;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UFUNCTION()
+	virtual void OnOverlapBegin(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep, 
+		const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	virtual void OnOverlapEnd(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex);
+
+};
