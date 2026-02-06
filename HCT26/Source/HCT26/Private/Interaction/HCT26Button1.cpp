b4e8eae4b8b65ff7e42b678798e0bb3ed3318de1
Khoi Van <vanthekhoi@gmail.com>
2026-02-06 23:29:46 +0700

    Spawn FX (Override OnOverlapBegin)

---
+++ HCT26/Source/HCT26/Private/Interaction/HCT26Button1.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interaction/HCT26Button1.h"
+
+
+// Sets default values
+AHCT26Button1::AHCT26Button1()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AHCT26Button1::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHCT26Button1::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHCT26Button1::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	if (OverlapEffect != nullptr)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, OverlapEffect, GetActorLocation());
+	}
+}
+
+void AHCT26Button1::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::OnOverlapEnd(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
+
---
+++ HCT26/Source/HCT26/Public/Interaction/HCT26Button1.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HCT26ButtonBase.h"
+#include "NiagaraFunctionLibrary.h"
+#include "HCT26Button1.generated.h"
+
+UCLASS()
+class HCT26_API AHCT26Button1 : public AHCT26ButtonBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AHCT26Button1();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="HoiChoTet|FX_Overlap")
+	TObjectPtr<UNiagaraSystem> OverlapEffect;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void OnOverlapBegin(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep, 
+		const FHitResult& SweepResult) override;
+	
+	virtual void OnOverlapEnd(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex) override;
+};
