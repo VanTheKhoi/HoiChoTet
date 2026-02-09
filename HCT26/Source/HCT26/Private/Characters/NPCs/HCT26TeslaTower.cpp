b30efc31ea50b9cbd8fce31768e5ddf0f438aab4
Khoi Van <vanthekhoi@gmail.com>
2026-02-09 22:58:36 +0700

    Add TeslaTower logic

---
+++ HCT26/Source/HCT26/Private/Characters/NPCs/HCT26TeslaTower.cpp
@@ -0,0 +1,74 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/NPCs/HCT26TeslaTower.h"
+
+
+// Sets default values
+AHCT26TeslaTower::AHCT26TeslaTower()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	// Create root scene component
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	RootComponent = SceneRoot;
+	
+	//Create mesh
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("BaseTeslaTowerMesh");
+	MeshComp->SetupAttachment(SceneRoot);
+	
+	// Create a Niagara component for shooting effect and attach to root
+	ShootEffect = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Shoot Effect Niagara Component"));
+	ShootEffect->SetupAttachment(SceneRoot);
+	
+	// Create a Niagara component for area effect and attach to root
+	AreaEffect = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Area Effect Niagara Component"));
+	AreaEffect->SetupAttachment(SceneRoot);
+	
+	// Initialize cached pawn pointer
+	CachedPlayerPawn = nullptr;
+}
+
+void AHCT26TeslaTower::UpdateAreaEffectParameters(float Radius, float Height, FLinearColor Color)
+{
+	if (AreaEffect != nullptr)
+	{
+		AreaEffect->SetFloatParameter(FName("Radius"), Radius);
+		AreaEffect->SetFloatParameter(FName("Height"), Height);
+		AreaEffect->SetColorParameter(FName("Color"), Color);
+	}
+}
+
+// Called when the game starts or when spawned
+void AHCT26TeslaTower::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Cache the player's pawn
+	if (const APlayerController* PlayerController = GetWorld()->GetFirstPlayerController())
+	{
+		CachedPlayerPawn = PlayerController->GetPawn();
+	}
+	
+}
+
+void AHCT26TeslaTower::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	UpdateAreaEffectParameters(200.f, 
+								200.f, 
+								FLinearColor::FromSRGBColor(FColor(255, 0, 0)));
+}
+
+// Called every frame
+void AHCT26TeslaTower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	if (ShootEffect != nullptr && CachedPlayerPawn != nullptr)
+	{
+		ShootEffect->SetVariablePosition(FName("PositionTarget"), CachedPlayerPawn->GetActorLocation());
+	}
+}
+
---
+++ HCT26/Source/HCT26/Public/Characters/NPCs/HCT26TeslaTower.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NiagaraComponent.h"
+#include "Components/MeshComponent.h"
+#include "HCT26TeslaTower.generated.h"
+
+UCLASS()
+class HCT26_API AHCT26TeslaTower : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AHCT26TeslaTower();
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Components)
+	TObjectPtr<USceneComponent> SceneRoot;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Components)
+	TObjectPtr<UStaticMeshComponent> MeshComp;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=VFX)
+	TObjectPtr<UNiagaraComponent> ShootEffect;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=VFX)
+	TObjectPtr<UNiagaraComponent> AreaEffect;
+	
+	UFUNCTION(BlueprintCallable, Category=TeslaTower)
+	void UpdateAreaEffectParameters(float Radius, float Height, FLinearColor Color);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	virtual void OnConstruction(const FTransform& Transform) override;
+	
+	// Cached reference to the player pawn
+	UPROPERTY(BlueprintReadOnly, Category=Player)
+	TObjectPtr<APawn> CachedPlayerPawn;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+};
