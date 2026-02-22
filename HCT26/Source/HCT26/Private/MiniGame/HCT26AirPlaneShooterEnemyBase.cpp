ea0e9f2f27db40db2750c7b6e5436fc2c46f7f17
Khoi Van <vanthekhoi@gmail.com>
2026-02-22 15:50:22 +0700

    [AirPlaneShooter] Update Enemy + Level changed

---
+++ HCT26/Source/HCT26/Private/MiniGame/HCT26AirPlaneShooterEnemyBase.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MiniGame/HCT26AirPlaneShooterEnemyBase.h"
+
+
+// Sets default values
+AHCT26AirPlaneShooterEnemyBase::AHCT26AirPlaneShooterEnemyBase()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	// Create root scene component
+	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+	
+	// Create, parent, and configure airplane mesh component
+	AirPlaneMesh = CreateDefaultSubobject<UStaticMeshComponent>("AirPlaneMesh");
+	AirPlaneMesh->SetupAttachment(SceneRoot);
+	
+	// Create, parent, and configure sphere collision component
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>("CollisionSphere");
+	CollisionComponent->SetupAttachment(SceneRoot);
+	CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CollisionComponent->SetCollisionObjectType(ECC_Pawn);
+	CollisionComponent->SetCollisionResponseToAllChannels(ECR_Block);
+	CollisionComponent->SetGenerateOverlapEvents(true);
+	
+	// Make sure hit events are enabled
+	CollisionComponent->SetNotifyRigidBodyCollision(true);
+	
+}
+
+// Called when the game starts or when spawned
+void AHCT26AirPlaneShooterEnemyBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CollisionComponent->OnComponentHit.AddDynamic(this, &AHCT26AirPlaneShooterEnemyBase::OnHit);
+}
+
+// Called every frame
+void AHCT26AirPlaneShooterEnemyBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AHCT26AirPlaneShooterEnemyBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AHCT26AirPlaneShooterEnemyBase::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (GEngine)
+	{
+		// Basic message
+		GEngine->AddOnScreenDebugMessage(
+			-1,                    // Key (-1 = always add new message)
+			5.0f,                  // Display time in seconds
+			FColor::Green,         // Text color
+			TEXT("Enemy Hit !!!")
+		);
+	}
+}
+
---
+++ HCT26/Source/HCT26/Public/MiniGame/HCT26AirPlaneShooterEnemyBase.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/SphereComponent.h"
+#include "HCT26AirPlaneShooterEnemyBase.generated.h"
+
+UCLASS()
+class HCT26_API AHCT26AirPlaneShooterEnemyBase : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AHCT26AirPlaneShooterEnemyBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Scene Root
+	UPROPERTY()
+	TObjectPtr<USceneComponent> SceneRoot;
+	
+	// Airplane Mesh
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UStaticMeshComponent> AirPlaneMesh;
+	
+	// Sphere Collision
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USphereComponent> CollisionComponent;
+	
+	// Function to handle hit events
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor,
+			   UPrimitiveComponent* OtherComp, FVector NormalImpulse,
+			   const FHitResult& Hit);
+
+};
