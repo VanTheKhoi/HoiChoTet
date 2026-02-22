23062a2e2ed010530cf34c994b81cef5836eda11
Khoi Van <vanthekhoi@gmail.com>
2026-02-22 15:44:40 +0700

    [AirPlaneShooter] Update Overlap Button

---
+++ HCT26/Source/HCT26/Private/Interaction/HCT26AirPlaneShooterButton.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interaction/HCT26AirPlaneShooterButton.h"
+#include "Core/GameLogs/GameLogsBase.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AHCT26AirPlaneShooterButton::AHCT26AirPlaneShooterButton()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AHCT26AirPlaneShooterButton::BeginPlay()
+{
+	Super::BeginPlay();
+	StartOverlap.AddDynamic(this, &AHCT26AirPlaneShooterButton::StartAirPlaneShooterGame);
+}
+
+// Called every frame
+void AHCT26AirPlaneShooterButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHCT26AirPlaneShooterButton::StartAirPlaneShooterGame(AActor* OverlappingActor)
+{
+	// Debug message
+	UE_LOG(HCT26GameLogs::LogHCT, Log, TEXT("Start AirPlaneShooter Game!"));
+	
+	// Broadcast the StartGame event with true to indicate the game should start
+	StartGame.Broadcast(true);
+}
+
---
+++ HCT26/Source/HCT26/Public/Interaction/HCT26AirPlaneShooterButton.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HCT26ButtonBase.h"
+#include "HCT26AirPlaneShooterButton.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSignatureStartGame, bool, IsStartGame);
+
+UCLASS()
+class HCT26_API AHCT26AirPlaneShooterButton : public AHCT26ButtonBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AHCT26AirPlaneShooterButton();
+	
+	UPROPERTY(BlueprintAssignable, Category=HoiChoTet)
+	FSignatureStartGame StartGame;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	// Start Game
+	UFUNCTION()
+	void StartAirPlaneShooterGame(AActor* OverlappingActor);
+};
