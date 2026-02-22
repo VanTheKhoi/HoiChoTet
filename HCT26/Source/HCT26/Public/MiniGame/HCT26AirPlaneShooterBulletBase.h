b63bf2fd4b8acaeb88c172d6506e902cfbd05f90
Khoi Van <vanthekhoi@gmail.com>
2026-02-22 22:03:16 +0700

    [AirPlaneShooter] Add Bullet

---
+++ HCT26/Source/HCT26/Public/MiniGame/HCT26AirPlaneShooterBulletBase.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HCT26AirPlaneShooterBulletBase.generated.h"
+
+UCLASS()
+class HCT26_API AHCT26AirPlaneShooterBulletBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AHCT26AirPlaneShooterBulletBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
