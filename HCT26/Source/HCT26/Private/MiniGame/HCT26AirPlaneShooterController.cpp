0ee4324d0160af323ba921ac7d67c38695e7a8ca
Khoi Van <vanthekhoi@gmail.com>
2026-04-07 22:41:02 +0700

    [Assigment-April-Day1] Basic sequence setup

--- HCT26/Source/HCT26/Private/MiniGame/HCT26AirPlaneShooterController.cpp
+++ HCT26/Source/HCT26/Private/MiniGame/HCT26AirPlaneShooterController.cpp
@@ -19,7 +19,6 @@ AHCT26AirPlaneShooterController::AHCT26AirPlaneShooterController()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	bIsCameraShaking = false;
 	
 	// Create root scene component
 	SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
---
+++ HCT26/Source/HCT26/Public/MiniGame/HCT26AirPlaneShooterGameWinMenuBase.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page o
+// f Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HCT26AirPlaneShooterGameWinMenuBase.generated.h"
+
+/**
+ * 
+ */
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnExitGameWinButtonClick);
+
+UCLASS()
+class HCT26_API UHCT26AirPlaneShooterGameWinMenuBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintCallable, Category="Events")
+	FOnExitGameWinButtonClick OnExitGameWinButtonClick;
+};
